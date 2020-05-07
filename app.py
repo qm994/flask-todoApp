de5a35778e273383c13694206a6f5efd378397e4
Qingyuan Ma <qm28@georgetown.edu>
2020-05-07 13:24:04 -0400

    changes

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 from flask import Flask, render_template
-
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = '''postgresql://qingyuan:MAzhang199711#@127.0.0.1:5432/todoapp'''
+db = SQLAlchemy(app)
+
+# todo: what models do we need to control application? 
+class Todo(db.Model):
+    __tablename__ = "todos"
+    id = db.Column(db.Integer, primary_key = True)
+    description = db.Column(db.String(), nullable = False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+# create all tables 
+db.create_all()
+
 @app.route('/')
 def index():
-    return render_template('index.html', data = [
-        {
-        "description": "Todo1"
-        },
-        {
-        "description": "Todo2"
-        },
-        {
-        "description": "Todo3"
-        }
-    ])
+    return render_template('index.html', data = Todo.query.all()
+    )
